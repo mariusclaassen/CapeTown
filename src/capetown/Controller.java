209c5e0f389f1f094f19ea28cf0c42c34dad395f
Marius Claassen <mariusclaassen@gmail.com>
2015-07-12 23:44:45 +0200

    Code cleanup

--- src/capetown/Controller.java
+++ src/capetown/Controller.java
@@ -12,19 +12,19 @@ public class Controller {
     @FXML private Button stadiumButton;
     @FXML private Button islandButton;
     @FXML private ImageView imageView;
+    @FXML private Button mountainButton;
     @FXML private Label textLabel;
 
     @FXML public void buttonClicked (ActionEvent event){
-        Image image;
          if(event.getSource().equals(stadiumButton)) {
-             imageView.setImage(image = new Image("file:src/capetown/Images/CapeTownStadium.jpg"));
+             imageView.setImage(new Image("file:src/capetown/Images/CapeTownStadium.jpg"));
              textLabel.setText("The 55,000-capacity stadium built for the 2010 football world cup.");
         } else if(event.getSource().equals(islandButton)) {
-            imageView.setImage(image = new Image("file:src/capetown/Images/RobbenIsland.jpg"));
+            imageView.setImage(new Image("file:src/capetown/Images/RobbenIsland.jpg"));
             textLabel.setText("The former prison island of South Africa where Nelson Mandela spent " +
                     "18 of his 27 years' imprisonment.");
-        } else {
-             imageView.setImage(image = new Image("file:src/capetown/Images/TableMountain.jpg"));
+        } else if(event.getSource().equals(mountainButton)) {
+             imageView.setImage(new Image("file:src/capetown/Images/TableMountain.jpg"));
              textLabel.setText("The flat-topped mountain is a prominent landmark overlooking Cape Town.");
         }
     }
